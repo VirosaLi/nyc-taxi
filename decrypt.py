356138e1f6a8f675362c78a3a6abf8d9a515f7e9
VirosaLi <fangchen.li@outlook.com>
2019-06-02 22:25:49 -0500

    "decryption script fix"

--- decrypt.py
+++ decrypt.py
@@ -2,30 +2,36 @@ from os import path, scandir, mkdir
 import json
 import pandas as pd
 from create_hashes import build_medallion_table, build_licence_table
-from decrypt_pandas import decrypt_medallion, decrypt_licence
+from decrypt_pandas import decrypt_md5
+
+medallion_table_path = 'medallion_table.txt'
+license_table_path = 'license_table.txt'
 
 # check if lookup tables have been created
-if path.exists('medallion_table.txt') and path.exists('licence_table.txt'):
-    print('decryption tables found, loading...\n')
+if path.exists(medallion_table_path) and path.exists(license_table_path):
+    print('Decryption tables found, loading...\n')
 
     # load md5 lookup table from txt file
-    with open('medallion_table.txt', 'r') as medallion, open('licence_table.txt', 'r') as licence:
+    with open(medallion_table_path, 'r') as medallion, open(license_table_path, 'r') as licence:
         medallion_table = json.load(medallion)
-        licence_table = json.load(licence)
+        license_table = json.load(licence)
 else:
-    print('decryption tables do not exist, creating...\n')
+    print('Decryption tables do not exist, creating...\n')
 
     medallion_table = build_medallion_table()
-    with open('medallion_table.txt', 'w') as output:
+    with open(medallion_table_path, 'w') as output:
         json.dump(medallion_table, output)
 
-    licence_table = build_licence_table()
-    with open('licence_table.txt', 'w') as output:
-        json.dump(licence_table, output)
+    license_table = build_licence_table()
+    with open(license_table_path, 'w') as output:
+        json.dump(license_table, output)
+
 
 # create folder for result
-if not path.exists('result'):
-    mkdir('result')
+if not path.exists('results'):
+    mkdir('results')
+
+print('Start decrypting dataset...\n')
 
 # iterate through data folder
 with scandir('data') as it:
@@ -35,11 +41,13 @@ with scandir('data') as it:
         df = pd.read_csv(dataset_path)
 
         # decrypt medallion and hack_license
-        df['medallion'] = df['medallion'].map(lambda x: decrypt_medallion(x))
-        df['hack_license'] = df['hack_license'].map(lambda x: decrypt_licence(x))
+        df['medallion'] = df['medallion'].map(lambda x: decrypt_md5(x, medallion_table))
+        df['hack_license'] = df['hack_license'].map(lambda x: decrypt_md5(x, license_table))
+
+        # print(df.head(10))
 
         # save result to file
-        result_path = path.join('result', 'decrypted_' + entry.name)
+        result_path = path.join('results', 'decrypted_' + entry.name)
         df.to_csv(result_path)
 
         print(f'dataset {path.basename(dataset_path)} decrypted.')
--- decrypt_pandas.py
+++ decrypt_pandas.py
@@ -2,45 +2,24 @@ import json
 import pandas as pd
 
 
-def decrypt_medallion(m):
-    """
-    decrypt medallion using a lookup table
-    :param m: the encrypt medallion
-    :return: the decrypt medallion if it's in the table, the original encrypt medallion if not
-    """
-    if m not in medallion_table:
-        # print('miss medallion: ', m)
-        return m
+def decrypt_md5(hashed, table):
+    if hashed not in table:
+        return hashed
     else:
-        return medallion_table[m]
-
-
-def decrypt_licence(m):
-    """
-    decrypt licence using a lookup table
-    :param m: the encrypt licence
-    :return: the decrypt licence if it's in the table, the original encrypt licence if not
-    """
-    if m not in licence_table:
-        # print('miss licence: ', m)
-        return m
-    else:
-        return licence_table[m]
-
-
-# read md5 lookup table from txt file
-with open('medallion_table.txt', 'r') as medallion, open('licence_table.txt', 'r') as licence:
-    medallion_table = json.load(medallion)
-    licence_table = json.load(licence)
+        return table[hashed]
 
 
 def main():
+    # read md5 lookup table from txt file
+    with open('medallion_table.txt', 'r') as medallion, open('licence_table.txt', 'r') as licence:
+        medallion_table = json.load(medallion)
+        licence_table = json.load(licence)
     # read taxi data from file using pandas
     df = pd.read_csv("trip_data_1.csv")
 
     # decrypt medallion and hack_license
-    df['medallion'] = df['medallion'].map(lambda x: decrypt_medallion(x))
-    df['hack_license'] = df['hack_license'].map(lambda x: decrypt_licence(x))
+    df['medallion'] = df['medallion'].map(lambda x: decrypt_md5(x, medallion_table))
+    df['hack_license'] = df['hack_license'].map(lambda x: decrypt_md5(x, licence_table))
 
     # print out the first 10 rows
     print(df.head(10))
